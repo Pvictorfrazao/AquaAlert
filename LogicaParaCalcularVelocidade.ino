a8a8e294f8491ef92d0d2ac50ba669976593aefe
Lucas <codesouzz@gmail.com>
2024-05-11 18:36:55 -0300

    Update LogicaParaCalcularVelocidade.ino
    Atualização para incluir a Pinagem dos sensores

--- LogicaParaCalcularVelocidade.ino
+++ LogicaParaCalcularVelocidade.ino
@@ -1,25 +1,48 @@
-#define DISTANCIA_ENTRE_SENSORES_CM 50
+const int startSensorEchoPin = 2; 
+const int endSensorEchoPin = 3;
 
-unsigned long startTime;
-unsigned long endTime;
-unsigned long timeDiff;
-float velocidade;
+unsigned long startTime = 0; 
+unsigned long endTime = 0;
+bool startDetected = false; 
+bool endDetected = false; 
+double velocidade; 
+double distSens = 50; // Distância entre os sensores
+double fatorParaSegundos = 1000.0; // Fator de conversão para segundos (milissegundos)
 
 void setup() {
   Serial.begin(9600);
+  pinMode(startSensorEchoPin, INPUT);
+  pinMode(endSensorEchoPin, INPUT);
 }
 
 void loop() {
-  startTime = micros();
-  delay(1000);
-  endTime = micros();
-  timeDiff = endTime - startTime;
+  // Lógica de detecção do sensor de início
+  if (digitalRead(startSensorEchoPin) == HIGH && !startDetected) {
+    startTime = millis();
+    startDetected = true;
+    Serial.println("Objeto detectado no sensor de início.");
+  }
 
-  velocidade = ((float)DISTANCIA_ENTRE_SENSORES_CM / ((float)timeDiff / 1000000.0)) * 3.6;
+  // Lógica de detecção do sensor de final
+  if (digitalRead(endSensorEchoPin) == HIGH && startDetected && !endDetected) {
+    endTime = millis();
+    endDetected = true;
+    Serial.println("Objeto detectado no sensor de final.");
+  }
 
-  Serial.print("Velocidade do objeto: ");
-  Serial.print(velocidade);
-  Serial.println(" km/h");
-
-  delay(1000);
+  // Cálculo da velocidade quando ambos os pontos foram detectados
+  if (startDetected && endDetected) {
+    double tempo = (endTime - startTime) / fatorParaSegundos; 
+    velocidade = (distSens / tempo) * 3.6; // Velocidade em km/h
+    Serial.print("Tempo: ");
+    Serial.print(tempo);
+    Serial.println("s");
+    Serial.print("Velocidade: ");
+    Serial.print(velocidade);
+    Serial.println("km/h");
+    
+    
+    startDetected = false;
+    endDetected = false;
+  }
 }
